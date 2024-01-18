e9f822ce0b0b9ea2e80aec618ae9f71593833aa1
Sohail Abbas <sohalidatoo@gmail.com>
2024-01-18 16:23:18 +0500

    InputComponent Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import InputBox from './components/InputBox';
 
 function App() {
-  return <h1 className="text-3xl bg-orange-500">Hello world!</h1>;
+  <InputBox />;
 }
 
 export default App;
